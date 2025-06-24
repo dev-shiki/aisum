75c7feb35eaf8df0d6f78f5897d217abd30c2c63
shiki <nandaleoardana@gmail.com>
2025-06-24 14:11:14 +0700

    chore: remove push.bat from repository and add to .gitignore

--- push.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Script push force ke GitHub dengan pesan otomatis (Windows)
-for /f "delims=" %%a in ('git config user.name') do set USERNAME=%%a
-for /f "delims=" %%a in ('wmic os get localdatetime ^| findstr /r /v "^$"') do set DATETIME=%%a
-set DATETIME=%DATETIME:~0,4%-%DATETIME:~4,2%-%DATETIME:~6,2% %DATETIME:~8,2%:%DATETIME:~10,2%:%DATETIME:~12,2%
-set MESSAGE=update: auto-push %DATETIME% by %USERNAME%
-git add .
-git commit -m "%MESSAGE%"
-git push --force 
